658f920a828917d63899988dea398191fb1c48e9
user <me@gmail.com>
2016-08-19 21:05:28 +0300

    Add sample of auxiliary Db assisting class

---
+++ app/Classes/DbAssist.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Classes;
+
+class DbAssist
+{
+    protected function query($query)
+    {
+        $res = mysql_query($query) or die(mysql_error());
+
+        if (!$res) {
+            return [];
+        }
+
+        if (true === $res) {
+            return [];
+        }
+
+        $result = [];
+        while ($row = mysql_fetch_assoc($res)) {
+            $result[] = $row;
+        }
+
+        return $result;
+    }
+    protected function safe($arg)
+    {
+        $arg = str_replace('\\"', '"', $arg);
+
+        return mysql_real_escape_string($arg);
+    }
+}
