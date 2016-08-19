266deea76a8619568778a233e3bfa957848c25a8
user <me@gmail.com>
2016-08-19 14:02:24 +0300

    Add sample of configuration file

---
+++ app/config.php.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+$routes = [
+    '/add' => 'AddController',
+    '/login' => 'LoginController',
+    '/admin' => 'AdminController',
+    '/logout' => 'LogoutController',
+    '/update' => 'UpdateController',
+    '/approve' => 'ApproveController',
+    '/messages' => 'MessagesController',
+    '/messages-all' => 'MessagesAllController',
+    '/' => 'ShowController',
+];
+
+$user = 'user';
+$password = 'password';
+$host = 'localhost';
+$db = 'db';
