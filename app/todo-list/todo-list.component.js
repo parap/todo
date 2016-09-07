60fedd4e1d8b8a432f918a24a771e8a07eeda2d9
user <me@gmail.com>
2016-09-07 10:17:23 +0300

    Clean daily task on creation

--- app/todo-list/todo-list.component.js
+++ app/todo-list/todo-list.component.js
@@ -32,6 +32,7 @@ angular.
                 $scope.add = function (text, type) {
                     $scope.todos.push({name: text, done: false, type: type});
                     $scope.newTodo = '';
+                    $scope.newDaily = '';
 
                     $http.post("/index.php?route=create", {"name": text, "type": type});
                 };
