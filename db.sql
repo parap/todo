dc709d70ac34f6797e721c8bca65b6277ed753f3
user <me@gmail.com>
2016-09-07 09:00:52 +0300

    Add SQL for daily table

--- db.sql
+++ db.sql
@@ -11,3 +11,10 @@ CREATE TABLE IF NOT EXISTS `item` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+CREATE TABLE IF NOT EXISTS `daily` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL,
+  `completed_at` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
