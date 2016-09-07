cc0e7851d3827c142605871f154556da7e1e528e
user <me@gmail.com>
2016-09-07 10:11:06 +0300

    Make constants string

--- app/Classes/ItemType.php
+++ app/Classes/ItemType.php
@@ -4,9 +4,9 @@ namespace Classes;
 
 class ItemType
 {
-    const Normal = 0;
-    const Daily = 1;
-    const Weekly = 2;
-    const Monthly = 3;
-    const Revolver = 4;
+    const Normal = "0";
+    const Daily = "1";
+    const Weekly = "2";
+    const Monthly = "3";
+    const Revolver = "4";
 }
