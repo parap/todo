aded846d58a6b273b32a68065788f57d151f3fff
parap <spalchinda@gmail.com>
2016-09-19 20:23:10 +0300

    Take into consideration for statistic tasks that were just added and thus have no completion dates

--- app/Classes/ItemRepo.php
+++ app/Classes/ItemRepo.php
@@ -189,7 +189,7 @@ class ItemRepo extends DbAssist {
         $query = "SELECT i.name , COUNT( d.completed_at ) AS number_completion, DATEDIFF(NOW(), i.created_at)+1 AS life_length
 FROM item i
 LEFT JOIN daily d ON ( i.id = d.item_id ) 
-WHERE (d.completed_at >=  '$date') AND i.type = '1' AND (i.archived_at = '0000-00-00' OR i.archived_at >= '$date')
+WHERE (d.completed_at >=  '$date' OR d.completed_at IS NULL) AND i.type = '1' AND (i.archived_at = '0000-00-00' OR i.archived_at >= '$date')
 GROUP BY i.id";
 
         $got = $this->query($query);
