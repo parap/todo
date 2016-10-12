0ca4cb5918bb897da53fadc4aeb7b50d22a716cf
parap <spalchinda@gmail.com>
2016-10-12 17:08:17 +0300

    Add sample of login component

--- app/auth/login.component.js
+++ app/auth/login.component.js
@@ -4,22 +4,23 @@ var login = {
     $inject: ['$scope', '$location', 'AuthenticationService'],
     templateUrl: 'app/auth/login.template.html',
     controller: function login($scope, $location, AuthenticationService) {
-        var vm = this;
-        vm.login = login;
+        var vm = $scope;
 
         (function initController() {
             // reset login status
             AuthenticationService.ClearCredentials();
         })();
 
-        function login() {
+        vm.login = function() {
+            
             vm.dataLoading = true;
+            
             AuthenticationService.Login(vm.username, vm.password, function (response) {
                 if (response.success) {
                     AuthenticationService.SetCredentials(vm.username, vm.password);
                     $location.path('/');
                 } else {
-                    alert(response.message);
+                    console.log('failure! ' + response.message);
                     vm.dataLoading = false;
                 }
             });
@@ -31,6 +32,5 @@ var login = {
 
 
 angular
-//        .module('todoApp')
         .module('login')
         .component('login', login);
