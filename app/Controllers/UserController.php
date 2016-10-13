ada1c8934577e78950d253350f4964d17c90db08
parap <spalchinda@gmail.com>
2016-10-13 12:07:10 +0300

    Save username to session

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -15,19 +15,28 @@ class UserController
         $this->repo = new UserRepo();
         $this->post = json_decode(file_get_contents('php://input'),true);
     }
+    
+    public function logout(Request $request)
+    {
+        unset($_SESSION['logged-email']);
+    }
 
     public function login(Request $request)
     {
         $result = $this->repo->login($this->post['email'], $this->post['ps']);
         
+        if ($result) {
+            $_SESSION['logged-email'] = $this->post['email'];
+        }
+        
         $params = ['success'=> false, 'message'=> 'wrong login'];
+        
         return $result ? json_encode(['success'=> true]) : json_encode($params);
     }
     
     public function register(Request $request)
     {
         $result = $this->repo->register($this->post['email'], $this->post['password']);
-        
         
         $params = ['success'=> false, 'message'=> 'User exists or other error'];
         return $result ? json_encode(['success'=> true]) : json_encode($params);
