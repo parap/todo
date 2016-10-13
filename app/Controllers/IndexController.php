5c2907f8d2e04ab2c8872142e1961f0b69a4645b
parap <spalchinda@gmail.com>
2016-10-13 13:18:42 +0300

    Extract getDay() mechanics to separate method

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -11,53 +11,65 @@ use Classes\ItemType;
  *
  * @author user
  */
-class IndexController 
+class IndexController
 {
+
     protected $repo;
     protected $post;
-    
+
     public function __construct()
     {
         $this->repo = new ItemRepo();
-        
+
         // json_decode($request->getContent()) can be used alternately
-        $this->post = json_decode(file_get_contents('php://input'),true);
+        $this->post = json_decode(file_get_contents('php://input'), true);
     }
-    
+
+    private function getDay()
+    {
+        // FIXME - remove ugly REQUEST_URI thing
+        if (!isset($_SERVER['REQUEST_URI'])) {
+            return 0;
+        }
+
+        $params = explode('=', $_SERVER['REQUEST_URI']);
+        $day    = count($params) > 1 ? (int) $params[1] : 0;
+        return $day;
+    }
+
     public function fetch(Request $request)
     {
-        // FIXME - remove ugly REQUEST_URI thing
-        $day = (isset($_SERVER['REQUEST_URI']) && count(explode('=', $_SERVER['REQUEST_URI'])) > 1) ? (int)explode('=', $_SERVER['REQUEST_URI'])[1] : 0;
-        $date = (new \DateTime($day.' day'))->format('Y-m-d');
+
+        $day     = $this->getDay();
+        $date    = (new \DateTime($day . ' day'))->format('Y-m-d');
         $results = $this->repo->fetch($date);
-        $json = json_encode($results);
-        
+        $json    = json_encode($results);
+
         return $json;
     }
-    
+
     public function fetchArchived(Request $request)
     {
-        // FIXME - remove ugly REQUEST_URI thing
-        $day = (isset($_SERVER['REQUEST_URI']) && count(explode('=', $_SERVER['REQUEST_URI'])) > 1) ? (int)explode('=', $_SERVER['REQUEST_URI'])[1] : 0;
-        $date = (new \DateTime($day.' day'))->format('Y-m-d');
+        $day     = $this->getDay();
+        $date    = (new \DateTime($day . ' day'))->format('Y-m-d');
         $results = $this->repo->fetchArchived($date);
-        $json = json_encode($results);
-        
+        $json    = json_encode($results);
+
         return $json;
     }
-    
+
     public function archive(Request $request)
     {
         $id = $this->post['id'];
         $this->repo->archive($id);
     }
-    
+
     public function unarchive(Request $request)
     {
         $id = $this->post['id'];
         $this->repo->unarchive($id);
     }
-    
+
     public function create(Request $request)
     {
         $name = $this->post['name'];
@@ -71,27 +83,28 @@ class IndexController
     public function update(Request $request)
     {
         $name = $this->post['name'];
-        $id = $this->post['id'];
+        $id   = $this->post['id'];
         $this->repo->update($id, $name);
     }
 
     public function complete(Request $request)
     {
-        $id = $this->post['id'];
+        $id   = $this->post['id'];
         $type = $this->post['type'];
-        $day = $this->post['day'];
-        $date = (new \DateTime($day.' day'))->format('Y-m-d');
-        
+        $day  = $this->post['day'];
+        $date = (new \DateTime($day . ' day'))->format('Y-m-d');
+
         $method = $this->post['done'] ? 'complete' : 'uncomplete';
         $this->repo->$method($id, $type, $date);
-        
+
         $results = $this->repo->findDelay($id);
-        
+
         return isset($results[0]['delay']) ? $results[0]['delay'] : '';
     }
-    
+
     public function render()
     {
         return file_get_contents('index.html');
     }
+
 }
