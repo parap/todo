fdbfdd805c01f9daeacac8e85a32961d47beaa7c
parap <spalchinda@gmail.com>
2016-10-31 00:31:07 +0200

    Add sample of switching to other language

--- app/menu/menu.component.js
+++ app/menu/menu.component.js
@@ -5,15 +5,22 @@
             module('menu').
             component('menu', {
                 templateUrl: 'app/menu/menu.template.html',
-                controller: function MenuController($scope, $rootScope, AuthenticationService) {
-                    
+                controller: function MenuController($scope, $rootScope,
+                        AuthenticationService, $translate) {
+
                     $rootScope.$on('$locationChangeStart', function () {
                         $rootScope.logged = AuthenticationService.GetUsername();
                         $scope.logged = $rootScope.logged;
                     });
-                    
+
                     $rootScope.logged = AuthenticationService.GetUsername();
                     $scope.logged = $rootScope.logged;
+                    $scope.language = $translate.use();
+
+                    $scope.changeLanguage = function (langKey) {
+                        $translate.use(langKey);
+                        $scope.language = $translate.use();
+                    };
                 }
             });
 }());
